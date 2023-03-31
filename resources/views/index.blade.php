fb76271abcc822d765a63ef49484453d3150a25d
z-fujimori <fuji.tai.29@i.softbank.jp>
2023-03-31 21:20:11 +0900

    コミットの試し

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>ララベル試し</title>
+    </head>
+    <body>
+        <h1>ララベル試しホーム</h1>
+
+        <img src="{{ asset('images/inu.jpeg') }}" alt="inu">
+
+        <h2>以下からページに飛べます</h2>
+        <a href="{{url('/ｐ１')}}">p1</a>
+        <a href="{{url('/ｐ２')}}">p2</a>
+        <a href="{{url('/ｐ３')}}">p3</a>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',function () {
+    return view('index');
+});
+
+Route::get('/ｐ１',function () {
+    return view('ｐ１');
+});
+
+Route::get('/ｐ２',function () {
+    return view('ｐ２');
+});
+
+Route::get('/ｐ３',function () {
+    return view('ｐ３');
+});
+
+?>
