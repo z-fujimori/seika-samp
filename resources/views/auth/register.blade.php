14ba521fb80439c8289d3c960075058567c31c80
Rei1449 <kiriya4673@gmail.com>
2023-04-15 16:38:27 +0900

    '見た目整え'

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">ユーザー登録</div>
+                <div class="card-header bg-primary">ユーザー登録</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
